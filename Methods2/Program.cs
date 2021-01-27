52db8772260afa8be20c50da18259b42e9828128
Hayri Can GÜNDOĞDU <hc_gundogdu@aof.anadolu.edu.tr>
2021-01-27 09:55:48 +0300

    update

--- Methods2/Program.cs
+++ Methods2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Methods2
 {
@@ -6,6 +7,7 @@ namespace Methods2
     {   //fonksiyonların çalıştırıldığı ve kullanıcıdan veri alınan yer Main
         static void Main(string[] args)
         {
+            
             //parametresiz kullanımı
             Add();
             //parametreli kullanımı
@@ -13,18 +15,22 @@ namespace Methods2
             //default kullanımı
             Add3(20);
 
-
-
             //Ref kullanımı
             int number1 = 20;                     
             int number2 = 100;                              //Mevzu tamamen degiskenin degeriyle ilgilidir.
             int result2 = Add4(ref number1,number2);        //üstteki number1 in burayla tek alakası metoda 20'yi koyması ve artık hicbir sekilde devreye girmemek seklinde olur.Baska hicbir bağı yok.
             Console.WriteLine(result2);
             Console.WriteLine(number1);                    //number1 in durumunu ogrenmek icin
-            Console.ReadLine();                           //Output'ta okuma ile yapılan islemleri gosteren satırı kaybetmeye yarıyor.
+            
+            //Params kullanımı
+            Console.WriteLine(Add5(2, 4, 6, 5, 8, 54));
+            Console.ReadLine();
+
+        
         }
 
         //parametresiz metod
+
         static void Add()
         {
             Console.WriteLine("Eklendi!");              
@@ -46,7 +52,7 @@ namespace Methods2
 
         //Ref keyword
         //burada,number1 de iki tane deger tanımlıyoruz ya, bunu yapma direkt olarak yukaridaki number1'in referansını kullan demek ref. Yani yukarıdakinin refransı 101 ise metodun icindeki 102,
-        static int Add4(ref int number1,int number2)                                                                                    //ama ref dedigimizde metodun icindeki number1'de 101 olur.
+        static int Add4(ref int number1,int number2)                                                                                    //ama ref dedigimizde yukaridaki number1'de 102 olur.
         {                               //ref eklemeden de result'a etki ediyor ama number1'i referans tip'e ceviremiyor.
             number1 = 30;              //bu islemin yukaridaki number1 ile hicbir iliskisi yok.Ama ben metodun icindeki deger degistiginde üstteki number1'i de etkilesin istiyorum diyorsanız, Ref eklemek gerekiyor.
             return number1 + number2;
@@ -66,8 +72,21 @@ namespace Methods2
         }
 
         //method overloading
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
 
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
+        }
 
+        //Params keyword
+        static int Add5(params int[] numbers)         //params ile bu sekilde istedigimiz kadar deger verebiliriz.
+        {
+            return numbers.Sum();                     //.Sum() ---> toplama metodu.
+        }
 
     }
 }
